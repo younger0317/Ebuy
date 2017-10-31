242223520284da104a9ace8889ca984d8398dc41
younger0317 <young_er_2008@163.com>
2017-10-31 23:00:31 +0800

    添加新member.jsp页面，member_userList,memberServlet(未完成)

---
+++ EasyBuy/src/com/ebuy/servlet/MemberServlet.java
@@ -0,0 +1,30 @@
+package com.ebuy.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * @author linbingyang
+ * @version 1.0 2017-10-31
+ * 后台控制器
+ *
+ */
+public class MemberServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doPost(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+}
